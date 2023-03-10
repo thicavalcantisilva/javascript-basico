49b10a43f9870cae939952b2f32222f310d539d2
thicavalcantisilva <thicavalcantisilva@gmail.com>
2023-03-10 14:54:35 -0300

    function

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var username = 'Thi Cavalcanti'
 document.getElementById('user_name').innerHTML = username
 
 
-// Declaração de variavel flexivel, com tipagem dinamica.
+// Declaração de variavel flexivel, com tipagem dinamica//
 
 //var nome = 'Mestre Yoda'
 //var idade = 100
@@ -15,3 +15,32 @@ document.getElementById('user_name').innerHTML = username
 //console.log (typeof idade)
 //console.log (typeof jedi)
 
+//Operadores Matematicos //
+
+//var n1 = 5
+//var n2 = 5
+
+//var total = n1 + n2 
+//console.log(total)
+
+
+//  Funções  //
+
+// function soma(n1, n2) {
+//     console.log(n1 + n2)
+// }
+
+// soma(5,5)
+
+// function BoasVindas(nome) {
+//     alert(nome + ', Sejam bem vindo')
+// } 
+// BoasVindas('Thiago')
+
+// function soma (n1, n2){
+//     return n1 + n2
+// }
+
+// var resultado = soma(2,2)
+
+// console.log(resultado)
