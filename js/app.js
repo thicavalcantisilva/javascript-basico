874e4739386af29db758fc4a77639367157461ca
thicavalcantisilva <thicavalcantisilva@gmail.com>
2023-03-10 13:58:20 -0300

    getElementyById

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ console.log('Olá, Javascript!')
 var username = 'Thi Cavalcanti'
 
 document.getElementById('user_name').innerHTML = username
-//
+
 
 // Declaração de variavel flexivel, com tipagem dinamica
 
