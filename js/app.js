332b55067843666e0adff4d0ce547688675cad4f
thicavalcantisilva <thicavalcantisilva@gmail.com>
2023-03-10 13:52:33 -0300

    Projeto-Starwars

---
+++ js/app.js
@@ -0,0 +1,16 @@
+console.log('Olá, Javascript!')
+
+var username = 'Thi Cavalcanti'
+
+document.getElementById('user_name').innerHTML = username 
+
+// Declaração de variavel flexivel, com tipagem dinamica
+
+//var nome = 'Mestre Yoda'
+//var idade = 100
+//var jedi = true
+
+//console.log (typeof nome)
+//console.log (typeof idade)
+//console.log (typeof jedi)
+
